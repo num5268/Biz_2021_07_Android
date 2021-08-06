98399c8816fb048f1a63c738d1521c11bed2d9f0
num5268 <num5268@gmail.com>
2021-08-06 10:49:49 +0900

    내도서관

---
+++ App02_Library/app/src/main/java/com/callor/library/config/Naver.java
@@ -0,0 +1,9 @@
+package com.callor.library.config;
+
+public class Naver {
+
+    public static String CLIENT_ID = "YOUR NAVER CLIENT ID";
+    public static String CLIENT_SECRET = "YOUR NAVER CLIENT SECRET";
+
+
+}
